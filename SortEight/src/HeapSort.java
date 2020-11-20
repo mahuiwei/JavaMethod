621ec128b22218dd29a5eb57066b0be1ffe34c61
https://github.com/mahuiwei/NewSort.git <1329248628@qq.com>
2020-11-20 14:23:24 +0800

    second-2020.11.20-mahuiwei

--- SortEight/src/HeapSort.java
+++ SortEight/src/HeapSort.java
@@ -11,8 +11,47 @@ public class HeapSort {
             System.out.println("第"+ i + "个数字为 ："+ tree[i]);
         }
     }
+    public void heapify(int tree[], int n, int i){
+        if (i >= n){
+            return;
+        }
+        int c1 = i * 2 + 1;//左子节点
+        int c2 = i * 2 + 2;//右子节点
+        int max = i;//假设父节点为三个当中的最大值
+        //找到最大值，找到最大值与i做交换，当i已经是最大值的时候，就不用做交换了
+        if (c1 < n && tree[c1] > tree[max]){
+            max = c1;
+        }
+        if (c2 < n && tree[c2] > tree[max]){
+            max = c2;
+        }
+        if (max != i){
+            swap(tree,max,i);
+            heapify(tree, n, max);
+        }
+    }
+    void swap(int tree[], int a, int b){
+        int temp;
+        temp = tree[a];
+        tree[a] = tree[b];
+        tree[b] = temp;
+    }
+    void build_heap(int tree[], int n){
+        int last_node = n - 1;
+        int parent_node = (last_node - 1) / 2;
+        for (int i = parent_node; i >= 0; i--){
+            heapify(tree, n, i);
+        }
+    }
+    void heap_sort(int tree[], int n){
+        build_heap(tree, n);
+        for (int i = n - 1; i >= 0; i--){
+            swap(tree, i , 0);
+            heapify(tree, i, 0);
+        }
+    }
 
-    public void heapfy(int tree[], int n, int i){
+/*    public void heapfy(int tree[], int n, int i){
         if (i >= n){
             return;
         }
@@ -52,6 +91,6 @@ public class HeapSort {
             swap(tree,i, 0);
             heapfy(tree ,i ,0);//砍断操作，默认就是堆减少一个（i代表的是当前这个树的节点个数）
         }
-    }
+    }*/
 
 }
---
+++ SortEight/src/QuickSort.java
@@ -0,0 +1,40 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int arr[] = {2,5,3,1,10,4};
+        QuickSort quickSort = new QuickSort();
+        quickSort.Quick_Sort(arr, 0,5);
+        int i;
+        for(i = 0; i < 6; i++){
+            System.out.println("第"+ i + "个数字为 ："+ arr[i]);
+        }
+    }
+    public void Quick_Sort(int arr[], int left, int right){
+        int l = left;
+        int r = right;
+        if(left > right){
+            return;
+        }
+
+        int temp = arr[left];
+        while(l < r){
+            while(temp <= arr[r] && l < r){
+                r--;
+            }
+            while(temp >= arr[l] && l < r){
+                l++;
+            }
+            if(l < r){
+                int t = arr[l];
+                arr[l] = arr[r];
+                arr[r] = t;
+            }
+        }
+        //最后将基准为i，和j相等位置的数字交换
+        arr[left] = arr[l];
+        arr[l] = temp;
+        //递归调用左半数组
+        Quick_Sort(arr, left,r - 1);
+        //递归调用右半数组
+        Quick_Sort(arr,l + 1, right);
+    }
+}
